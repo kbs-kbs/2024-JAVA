b8bb96e8a9384261dc896a713379d4b61b2b2885
kbs-kbs <yaglus58@gmail.com>
2024-12-13 09:46:07 +0900

    Update ScanDigit.java

--- presentation/ScanDigit.java
+++ presentation/ScanDigit.java
@@ -1,8 +1,8 @@
 package pack1;
 
-public class ScanDigit extends ScanNumber {
+public class ScanDigit extends ScanNumber { // ScanNumber 클래스를 상속받음
 
 	public void scanDigit(int num) {
-		super.scanNumber(0, 10, num);
+		super.scanNumber(0, 10, num); // 부모 클래스의 scanNumber(0, 10, num) 실행
 	}
 }
